e5af0265bfa669d016b53f0cc83971143f006c1f
Zooku <zacquiri@hotmail.com>
2023-08-21 16:16:37 +1000

    api hopefully

---
+++ api/get-number.js
@@ -0,0 +1,10 @@
+module.exports = async (req, res) => {
+  try {
+    const data = await fs.readFile('number.json', 'utf-8');
+    const number = JSON.parse(data).number;
+    res.json({ number });
+  } catch (error) {
+    console.error('Error reading number:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
