d83db00355625367e8a3cb6df0bb4e764db0c0c4
Zooku <zacquiri@hotmail.com>
2023-08-22 02:36:57 +1000

    reset basically

--- api/get-number.js
+++ api/get-number.js
@@ -1,13 +1,14 @@
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', 'https://card-rating2-o9k1ycggc-zacquiri.vercel.app'); // Replace with your frontend's domain
+    res.setHeader('Access-Control-Allow-Origin', 'https://card-rating2-zacquiri.vercel.app'); // Replace with your frontend's domain
     // ...
-  try {
-    const data = await fs.readFile('number.json', 'utf-8');
-    const number = JSON.parse(data).number;
-    res.json({ number });
-  } catch (error) {
-    console.error('Error reading number:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
+      try {
+        const data = await fs.readFile('number.json', 'utf-8');
+        const number = JSON.parse(data).number;
+        res.json({ number });
+      } catch (error) {
+        console.error('Error reading number:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    };
+    
--- api/update-number.js
+++ api/update-number.js
@@ -1,18 +1,18 @@
 module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', 'https://card-rating2-o9k1ycggc-zacquiri.vercel.app'); // Replace with your frontend's domain
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+    //res.setHeader('Access-Control-Allow-Origin', 'https://card-rating2-zacquiri.vercel.app'); // Replace with your frontend's domain
     // ...
-    const newNumber = req.body.number;
-    if (newNumber >= 0 && newNumber <= 9) {
-      try {
-        await fs.writeFile('number.json', JSON.stringify({ number: newNumber }));
-        res.sendStatus(200);
-      } catch (error) {
-        console.error('Error updating number:', error);
-        res.status(500).json({ error: 'Internal server error' });
+      const newNumber = req.body.number;
+      if (newNumber >= 0 && newNumber <= 9) {
+        try {
+          await fs.writeFile('number.json', JSON.stringify({ number: newNumber }));
+          res.sendStatus(200);
+        } catch (error) {
+          console.error('Error updating number:', error);
+          res.status(500).json({ error: 'Internal server error' });
+        }
+      } else {
+        res.status(400).json({ error: 'Invalid number' });
       }
-    } else {
-      res.status(400).json({ error: 'Invalid number' });
-    }
-  };
-  
+    };
+    
